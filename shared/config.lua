8f9e7349ce7ef85fb4b33923d7d9d95379c3ff16
Sarmingoo <78432708+Sarmingo@users.noreply.github.com>
2024-01-12 13:01:08 +0100

    Update config.lua

--- shared/config.lua
+++ shared/config.lua
@@ -6,8 +6,7 @@ Config.Rent = {
     ["rent1"] = {
         --TARGET
         ped = 'a_m_m_og_boss_01',
-        pedcoords = vector3(430.24, 3474.44, 34.56),
-        pedheading = 37.6,
+        pedcoords = vector4(430.24, 3474.44, 34.56, 37.6),
         label = 'Open rent',
         distance = 3.5, 
         icon = 'fas fa-car',
@@ -26,7 +25,7 @@ Config.Rent = {
                 event = 'spawncar',
                 arrow = true,
                 image = 'https://cdn.discordapp.com/attachments/954465466435403826/1127885708200263780/Futo-GTAVe-front.webp',
-                args = {model = 'futo', price = 250, spawncoords = vector3(433.68, 3480.04, 34.64), spawnheading = 289.08, account = 'money'}
+                args = {model = 'futo', price = 250, spawncoords = vector3(433.68, 3480.04, 34.64, 289.08), account = 'money'}
             },
             {
                 title = 'Blista',
@@ -35,7 +34,7 @@ Config.Rent = {
                 event = 'spawncar',
                 arrow = true,
                 image = 'https://cdn.discordapp.com/attachments/954465466435403826/1127887154589212722/Blista-GTAV-front.webp',
-                args = {model = 'blista', price = 350, spawncoords = vector3(433.68, 3480.04, 34.64), spawnheading = 289.08, account = 'money'}
+                args = {model = 'blista', price = 350, spawncoords = vector3(433.68, 3480.04, 34.64, 289.08), account = 'money'}
             }
         }
     },
